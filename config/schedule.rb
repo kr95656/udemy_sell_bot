0ff62bbcbcb731bcadf125596d37bc31fadb9906
kr95656 <katmsg8239@gmail.com>
2020-06-27 23:44:53 +0900

    Regular message

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,17 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+
+rails_env = ENV['RAILS_ENV'] || :development
+
+set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
+
+# every 1.day, at: ['7:00 am'] do # タスクを処理するペースを記載する。（例は毎朝7時に実行）
+every 1.minutes do
+  rake 'push_line:push_line_message_morning'
+end
+
+#最初は「every 10.minutes do」（10分ごと）とかにして動作するか確かめました。
