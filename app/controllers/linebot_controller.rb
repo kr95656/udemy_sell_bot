6e004a1499e08cd1c65b60a7cf04f01653a044c2
kr95656 <katmsg8239@gmail.com>
2020-07-11 23:21:15 +0900

    delete comment out

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -22,51 +22,6 @@ class LinebotController < ApplicationController
     }
     "OK"
   end
-
-  # require 'line/bot'  # gem 'line-bot-api'
-
-  # # callbackアクションのCSRFトークン認証を無効
-  # protect_from_forgery :except => [:callback]
-
-  # def client
-  #   @client ||= Line::Bot::Client.new { |config|
-  #     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-  #     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  #   }
-  # end
-
-  # def callback
-  #   body = request.body.read
-
-  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
-  #   unless client.validate_signature(body, signature)
-  #     head :bad_request
-  #   end
-
-  #   events = client.parse_events_from(body)
-
-  #   events.each { |event|
-  #     case event
-  #     when Line::Bot::Event::Message
-  #       case event.type
-  #       when Line::Bot::Event::MessageType::Text
-  #         message = {
-  #           type: 'text',
-  #           text: event.message['text']
-  #         }
-  #       end
-  #     end
-  #   }
-
-  #   head :ok
-  # end
-
-
-
-
-
-
-
 end
 
 
