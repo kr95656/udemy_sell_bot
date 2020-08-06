31fe18f2e9240b38b813ddff905adf5f86398a4c
kr95656 <katmsg8239@gmail.com>
2020-08-06 14:59:12 +0900

    initial commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,6 +1,24 @@
 class LinebotController < ApplicationController
+  require 'line/bot'  # gem 'line-bot-api'
+
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
   def callback
     body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head :bad_request
+    end
+
     events = client.parse_events_from(body)
 
     events.each { |event|
@@ -20,17 +38,10 @@ class LinebotController < ApplicationController
           userId = event['source']['userId']  
           user = User.find_by(uid: userId)
           user.destroy if user.present?
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
         end
       end
     }
-    "OK"
+
+    head :ok
   end
 end
-
-#lineデベロッパーの設定において、ボットにグループチャットへの参加を許可した 
-
-
