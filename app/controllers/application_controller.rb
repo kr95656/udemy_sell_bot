43bcb353fb28cf811fbfe643e69ec48e04f37cdb
kr95656 <katmsg8239@gmail.com>
2020-08-06 15:15:23 +0900

    comment out applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
-  require 'line/bot' #gem 'line-bot-api'を使用
-  protect_from_forgery with: :null_session #callbackアクションのCSRFトークン認証を無効
-  before_action :validate_signature
+  # require 'line/bot' #gem 'line-bot-api'を使用
+  # protect_from_forgery with: :null_session #callbackアクションのCSRFトークン認証を無効
+  # before_action :validate_signature
 
-  def validate_signature
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-  end
+  # def validate_signature
+  #   body = request.body.read
+  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #   unless client.validate_signature(body, signature)
+  #     error 400 do 'Bad Request' end
+  #   end
+  # end
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
+  # def client
+  #   @client ||= Line::Bot::Client.new { |config|
+  #     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+  #     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  #   }
+  # end
 end
